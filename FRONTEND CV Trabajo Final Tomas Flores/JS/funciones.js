ae352a44b2a2d95de0f4c15c78f05def89a40efb
TommyCode99 <126628444+TommyCode99@users.noreply.github.com>
2023-03-24 16:59:27 -0300

    Add files via upload

--- FRONTEND CV Trabajo Final Tomas Flores/JS/funciones.js
+++ FRONTEND CV Trabajo Final Tomas Flores/JS/funciones.js
@@ -1,3 +1,29 @@
+let BS;
 document.getElementById('botonDarkMode').addEventListener('click',function(){
-    document.body.style.backgroundColor= '#15202B';
+    if (BS != 0) {
+        document.body.style.backgroundColor= '#15202B';
+        document.getElementById('botonDarkMode').innerHTML = "Light Mode";
+        document.getElementById('botonDarkMode').setAttribute('class', 'btn bg-light');
+        document.getElementById('nav').setAttribute('class', 'navbar navbar-expand-sm bg-dark');
+        document.getElementById('columnas').setAttribute('class', 'col-sm-4 col-md-3 col-lg-3 bs-body-color-rgb(21,32,43)');
+        document.getElementById('cuerpo').setAttribute('class','p-3 col-sm-8 col-md 9 col-lg-9 bg-dark');
+        document.getElementById('Conocimientos').setAttribute('class','text-info fw-bold p-1');
+        document.getElementById('Experiencia').setAttribute('class','text-info fw-bold p-1');
+        document.getElementById('seccion1').setAttribute('class','text-light');
+        document.getElementById('seccion2').setAttribute('class','text-light');
+        BS = 0;
+    } else {
+        document.body.style.backgroundColor= '#ced4da';
+        document.getElementById('botonDarkMode').innerHTML = "Dark Mode";
+        document.getElementById('botonDarkMode').setAttribute('class', 'btn bg-dark-subtle');
+        document.getElementById('nav').setAttribute('class', 'navbar navbar-expand-sm bg-dark');
+        document.getElementById('columnas').setAttribute('class', 'col-sm-4 col-md-3 col-lg-3 bg-dark-subtle');
+        document.getElementById('cuerpo').setAttribute('class','p-3 col-sm-8 col-md 9 col-lg-9 bg-secondary-subtle');
+        document.getElementById('Conocimientos').setAttribute('class','text-info-emphasis fw-bold p-1');
+        document.getElementById('Experiencia').setAttribute('class','text-info-emphasis fw-bold p-1');
+        document.getElementById('seccion1').setAttribute('class','');
+        document.getElementById('seccion2').setAttribute('class','');
+        BS = 1;
+    }
 })
+
